240554f646ad28c24e77bde9d45054c12afd51ab
Rubens Matheus <112494717+RubensMatheus-me@users.noreply.github.com>
2023-10-27 23:57:13 -0300

    Update arq-sim.c

--- arq-sim.c
+++ arq-sim.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
@@ -13,139 +12,130 @@ typedef int bool;
 #define SYSCALLINT 2
 #define SYSCALLNEWLINE 3
 
-int index =1;
+int index = 1;
 
-const char* get_reg_name_str (uint16_t reg)
-{
-	static const char *str[] = {
-		"r0",
-		"r1",
-		"r2",
-		"r3",
-		"r4",
-		"r5",
-		"r6",
-		"r7"
-	};
+const char* get_reg_name_str(uint16_t reg) {
+    static const char *str[] = {
+        "r0",
+        "r1",
+        "r2",
+        "r3",
+        "r4",
+        "r5",
+        "r6",
+        "r7"
+    };
 
-	return str[reg];
+    return str[reg];
 }
 
 typedef struct instructions {
-	uint16_t format;
-	uint16_t opcode;
-	uint16_t destiny;
-	uint16_t op1;
-	uint16_t op2;
-	uint16_t opI;
+    uint16_t format;
+    uint16_t opcode;
+    uint16_t destiny;
+    uint16_t op1;
+    uint16_t op2;
+    uint16_t imediate;
+} Instructions;
 
-}Instructions;
-
-uint16_t extract_bits (uint16_t v, uint8_t bstart, uint8_t blength);
-uint16_t memory[MEM_SIZE]; //memoria
-uint16_t registers[8]; //8 registradores disponiveis
+uint16_t extract_bits(uint16_t v, uint8_t bstart, uint8_t blength);
+uint16_t memory[MEM_SIZE];
+uint16_t registers[8];
 uint16_t pc;
 uint16_t instruction;
 
-
 uint16_t getMemory(int i) {
-	return memory[i];
+    i++;
+    return memory[i];
 }
-
 
 Instructions decode(uint16_t codedInstruction) {
-	Instructions decoded;
-	
-	switch (decoded.format) {
-		case 1: //formato I
-			decoded.opI = extract_bits(codedInstruction, 10, 2);	
-	}
-	decoded.format = extract_bits(codedInstruction, 15, 1); //formato
-	decoded.opcode = extract_bits(codedInstruction, 9, 6); //opcode
-	decoded.destiny = extract_bits(codedInstruction, 8, 3); //registrador 1
-	decoded.op1 = extract_bits(codedInstruction, 5, 3); //registrador 2
-	decoded.op2 = extract_bits(codedInstruction, 2, 3); //registrador 3
-	
-	registers[decoded.destiny] = decoded.destiny;
-	registers[decoded.op1] = decoded.op1;
-	registers[decoded.op2] = decoded.op2; 
-	
-	return decoded;
+    Instructions decoded;
+    decoded.format = extract_bits(codedInstruction, 15, 1);
 
+    switch (decoded.format) {
+        case 0: // formato R
+            decoded.opcode = extract_bits(codedInstruction, 9, 6);
+            decoded.destiny = extract_bits(codedInstruction, 8, 3);
+            decoded.op1 = extract_bits(codedInstruction, 5, 3);
+            decoded.op2 = extract_bits(codedInstruction, 2, 3);
+            break;
+        case 1: // formato I
+            decoded.imediate = extract_bits(codedInstruction, 10, 2);
+            decoded.opcode = extract_bits(codedInstruction, 9, 6);
+            decoded.destiny = extract_bits(codedInstruction, 8, 3);
+            break;
+    }
+
+    return decoded;
 }
 
+void syscall() {
+    switch (registers[0]) {
+        case SYSCALLEXIT:
+            exit(0);
+            break;
+    }
+}
 
-	
-int main (int argc, char **argv) {
-	
-	memory[1] = 0b0000001111111010; // 0 000001 111 111 010 sub
-	
-	pc = getMemory(1);
-	instruction = pc;
-	while(1) {
-		
-	}
-	
-	decode(instruction);
-	
-	
-	
-	
-	/*
-	uint8_t formatM0 = extract_bits(memory[0], 15, 1); //formato
-	uint8_t opcodeM0 = extract_bits(memory[0], 9, 6); //opcode
-	
-	instruction.format = extract_bits(memory[0], 15, 1); //formato
-	instruction.opcode = extract_bits(memory[1], 9, 6); //opcode
-	instruction.destiny = extract_bits(memory[1], 8, 3); //registrador 1
-	instruction.op1 = extract_bits(memory[1], 5, 3); //registrador 2
-	instruction.op2 = extract_bits(memory[1], 2, 3); //registrador 3
-	registers[instruction.destiny] = instruction.destiny;
-	registers[instruction.op1] = instruction.op1;
-	registers[instruction.op2] = instruction.op2;
-	
-	
-	printf("formato: %u\n", instruction.format);
-	printf("opcode: %u\n", instruction.opcode);
-	printf("r1: %u\n", registers[instruction.destiny]);
-	printf("r2: %u\n", registers[instruction.op1]);
-	printf("r3: %u\n", registers[instruction.op2]);
-	
-	
-	
-	switch (instruction.format) {
-		case 0:
-			switch (instruction.opcode) {
-				case 0:
-					registers[instruction.destiny] = registers[instruction.op1] + registers[instruction.op2];
-					printf("Valor do opcode: %u, soma: %u\n", instruction.opcode, registers[instruction.destiny]);
-					break;
-					
-				case 1:
-					registers[instruction.destiny] = registers[instruction.op1] - registers[instruction.op2];
-					printf("Valor do opcode: %u, subtracao: %u\n", instruction.opcode, registers[instruction.destiny]);	
-					break;
-					
-				case 2:
-					registers[instruction.destiny] = registers[instruction.op1] * registers[instruction.op2];
-					printf("Valor do opcode: %u, multiplicacao: %u\n", instruction.opcode, registers[instruction.destiny]);	
-					break;
-					
-				case 3:
-					registers[instruction.destiny] = registers[instruction.op1] / registers[instruction.op2];
-					printf("Valor do opcode: %u, divisao: %u\n", instruction.opcode, registers[instruction.destiny]);	
-					break;
-			}	
-	}	
-	*/
-	
-	
-	
-	if (argc != 2) {
-		printf("usage: %s [bin_name]\n", argv[0]);
-		exit(1);
-	}
-	
+void execute(Instructions instructions) {
+    switch (instructions.format) {
+        case 0: // formato R
+            switch (instructions.opcode) {
+                case 0:
+                    registers[instructions.destiny] = registers[instructions.op1] + registers[instructions.op2];
+                    printf("Valor do opcode: %u, soma: %u\n", instructions.opcode, registers[instructions.destiny]);
+                    break;
+                case 1:
+                    registers[instructions.destiny] = registers[instructions.op1] - registers[instructions.op2];
+                    printf("Valor do opcode: %u, subtracao: %u\n", instructions.opcode, registers[instructions.destiny]);
+                    break;
+                case 2:
+                    registers[instructions.destiny] = registers[instructions.op1] * registers[instructions.op2];
+                    printf("Valor do opcode: %u, multiplicacao: %u\n", instructions.opcode, registers[instructions.destiny]);
+                    break;
+                case 3:
+                    registers[instructions.destiny] = registers[instructions.op1] / registers[instructions.op2];
+                    printf("Valor do opcode: %u, divisao: %u\n", instructions.opcode, registers[instructions.destiny]);
+                    break;
+            }
+            break;
+        case 1: // formato I
+            switch (instructions.opcode) {
+                case 0: // jump
+                    pc = instructions.imediate;
+                    break;
+                case 1: // jump condicional
+                    if (registers[instructions.destiny]) {
+                        pc = instructions.imediate;
+                    }
+                    break;
+                case 3: // Mover valor imediato para o registrador destiny
+                    registers[instructions.destiny] = instructions.imediate;
+                    printf("Movido %u para %s\n", instructions.imediate, get_reg_name_str(instructions.destiny));
+                    break;
+            }
+            break;
+    }
+}
 
-	return 0;
+int main(int argc, char **argv) {
+
+    while (1) {
+        instruction = getMemory(pc);
+        Instructions decoded = decode(instruction);
+        if (decoded.opcode == SYSCALL) {
+            syscall();
+            break;
+        }
+        execute(decoded);
+        pc++;
+    }
+
+    if (argc != 2) {
+        printf("usage: %s [bin_name]\n", argv[0]);
+        exit(1);
+    }
+
+    return 0;
 }
